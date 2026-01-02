f93a29e8679fafe3f270bdcaf3c959cc30922b0a
Surender K M <85400951+SurenderKM21@users.noreply.github.com>
2026-01-02 15:08:45 +0530

    BFS implemented

--- Bfs.java
+++ Bfs.java
@@ -1,6 +1,31 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 public class Bfs{
+    public static void bfs(int V, ArrayList<ArrayList<Integer>> al)
+    {
+        boolean visited[] = new boolean[V+1];
+        Queue<Integer> q = new LinkedList<>();
+        
+        q.offer(1);
+        visited[1] = true;
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+            System.out.print(node+" ");
+
+            for(int i : al.get(node))
+            {
+                if(!visited[i])
+                {
+                    q.offer(i);
+                    visited[i] = true;
+                }
+            }
+        }
+
+    }
     public static void main(String... args)
     {
         //Number of vertices
@@ -15,29 +40,28 @@ public class Bfs{
         //Node 1
         al.get(1).addAll(Arrays.asList(2,3,4));
 
-        //Node 2 - no elements
+        //Node 2 
+        al.get(2).add(1);
         
         //Node 3 
-        al.get(3).addAll(Arrays.asList(5,6));
+        al.get(3).addAll(Arrays.asList(1,5,6));
 
-        //Node 4 - no elements
+        //Node 4 
+        al.get(4).add(1);
 
         //Node 5
-        al.get(5).add(7);
+        al.get(5).addAll(Arrays.asList(3,7));
 
         //Node 6
-        al.get(6).add(8);
+        al.get(6).addAll(Arrays.asList(3,8));
 
-        //Node 7 - no elements
-        //Node 8 - no elements
+        //Node 7 
+        al.get(7).add(5);
 
-        
-        for(ArrayList<Integer> a : al)
-        {
-            for(int i : a)
-                System.out.print(i+" ");
-            System.out.println();
-        }
+        //Node 8 
+        al.get(8).add(6);
+
         System.out.println("BFS traversal : ");
+        bfs(n,al);
     }
 }
